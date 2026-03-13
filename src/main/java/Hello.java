225d6ee49faa5e1c1141015a13c3859802155af2
rlaehddnwkd1107-maker <rlaehddnwkd1107@gmail.com>
2026-03-13 15:15:54 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello!! My First Java coding!!");
+        System.out.println("I want to go to home");
+        System.out.println("I need more caffeine");
+        System.out.println("I love you");
+        System.out.println("I'm so tired");
+        System.out.println("My name is DongWoo");
+
+    }
+}
