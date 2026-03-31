d8fd107218aa35354cadc8cb882452262a3a2dc4
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-03-31 11:17:34 +0900

    Code05exam

--- src/main/java/Exam52.java
+++ src/main/java/Exam52.java
@@ -9,11 +9,11 @@ public class Exam52 {
         System.out.println("숫자를 입력하세요 ==>");
         num = dw.nextLong();
 
-        if (num % 15 == 0){
+        if ( num % 15 == 0 ){
             System.out.println("3의배수와 5의 배수입니다.");}
-        else if ( num % 3 == 0){
+        else if ( num % 3 == 0 ){
             System.out.println("3의배수입니다.");}
-        else if ( num % 5 == 0){
+        else if ( num % 5 == 0 ){
             System.out.println("5의배수입니다.");}
         else {
             System.out.println("두수의 배수가 아닙니다.");}
