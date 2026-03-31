0ed9c602211783f5cf353349a7c85bd495fcaf1e
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-03-31 10:34:19 +0900

    Code05test

---
+++ src/main/java/Code05_test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Code05_test {
+    public static void main(String[] args){
+        Scanner dw = new Scanner(System.in);
+        int num;
+
+        num = dw.nextInt();
+
+        if(num%2 == 0){
+            System.out.println("짝수입니다.");
+
+        }
+        else{
+            System.out.println("홀수입니다.");
+        }
+
+        dw.close();
+    }
+}
