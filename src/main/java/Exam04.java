d94baee889e921d74d242b7add8adb68e97b708c
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-07 17:31:23 +0900

    exam

---
+++ src/main/java/Exam04.java
@@ -0,0 +1,19 @@
+public class Exam04 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            int star;
+
+            if (i <= 4) {
+                star = i;
+            } else {
+                star = 8 - i;
+            }
+
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
