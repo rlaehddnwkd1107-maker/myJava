021eab264c72b4fbd06b39c49ec6ccb2414d1f03
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-17 15:06:21 +0900

    Code07_06

--- src/main/java/Code07_06.java
+++ src/main/java/Code07_06.java
@@ -4,8 +4,8 @@ class Rabbit{
     int yPos;
 
 
-    Rabbit(){
-        shape = "토끼";
+    Rabbit(String value){
+        shape = value;
     }
     void setPosition(int x, int y){
         xPos = x;
@@ -15,20 +15,22 @@ class Rabbit{
 
 public class Code07_06 {
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit();
-        Rabbit rabbit2 = new Rabbit();
-        Rabbit rabbit3 = new Rabbit();
+        Rabbit rabbit1 = new Rabbit("원");
+        Rabbit rabbit2 = new Rabbit("삼각형");
+        Rabbit rabbit3 = new Rabbit("토끼");
 
-        rabbit1.shape = "원";
-        rabbit2.shape = "삼각형";
-        rabbit3.shape = "토끼";
+//        rabbit1.shape = "원";
+//        rabbit2.shape = "삼각형";
+//        rabbit3.shape = "토끼";
 
         rabbit1.setPosition(100, 100);
         rabbit2.setPosition(-100, 100);
         rabbit3.setPosition(0, -100);
 
-        Rabbit rabbit = new Rabbit();
+//        Rabbit rabbit = new Rabbit();
 
-        System.out.printf("토끼 객체의 기본모양은 [%s]입니다.", rabbit.shape);
+        System.out.printf("토끼1 객체의 기본모양은 [%s]입니다.", rabbit1.shape);
+        System.out.printf("토끼2 객체의 기본모양은 [%s]입니다.", rabbit2.shape);
+        System.out.printf("토끼3 객체의 기본모양은 [%s]입니다.", rabbit3.shape);
     }
 }
