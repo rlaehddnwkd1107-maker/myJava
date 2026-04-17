ad6d8305d537ebd19c611c626c57e269a37773a8
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-17 14:21:23 +0900

    Code07_06

---
+++ src/main/java/Code07_06.java
@@ -0,0 +1,20 @@
+class Rabbit{
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code07_06 {
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+
+    }
+}
