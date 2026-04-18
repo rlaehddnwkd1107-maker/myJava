016f692f1938fa9fc86d52075ea38a24f415b916
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-18 16:23:45 +0900

    exer

---
+++ src/main/java/exer/Q5.java
@@ -0,0 +1,15 @@
+package exer;
+
+public class Q5 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 100; i <= 300; i++) {
+            if (i % 5 == 0 && i % 7 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
