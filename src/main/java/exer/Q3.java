0ab3c87234236025bea4c1870e25a8f50126ce27
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-23 11:41:02 +0900

    중간고사

--- src/main/java/exer/Q3.java
+++ src/main/java/exer/Q3.java
@@ -1,16 +1,16 @@
-package exer;
-
-public class Q3 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-}
+//package exer;
+//
+//public class Q3 {
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 4; i++) {
+//            for (int j = 1; j <= 2 * i - 1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
 //public class Q3 {
 //    public static void main(String[] args) {
 //
