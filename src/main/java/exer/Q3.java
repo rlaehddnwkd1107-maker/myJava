9e1e8be5e57fcb979b233954c20e49416bf9b68f
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-04-23 11:37:35 +0900

    중간고사

--- src/main/java/exer/Q3.java
+++ src/main/java/exer/Q3.java
@@ -1,26 +1,26 @@
-//package exer;
-//
-//public class Q3 {
-//    public static void main(String[] args) {
-//        for (int i = 1; i <= 4; i++) {
-//            for (int j = 1; j <= 2 * i - 1; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//}
+package exer;
+
 public class Q3 {
     public static void main(String[] args) {
-
-        String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200";
-
-        int start = log.indexOf("GET ") + 4;
-        int end = log.indexOf(" HTTP");
-
-        String url = log.substring(start, end);
-
-        System.out.println(url);
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+
 }
+//public class Q3 {
+//    public static void main(String[] args) {
+//
+//        String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200";
+//
+//        int start = log.indexOf("GET ") + 4;
+//        int end = log.indexOf(" HTTP");
+//
+//        String url = log.substring(start, end);
+//
+//        System.out.println(url);
+//    }
+//}
---
+++ src/main/java/exer/Q6.java
@@ -0,0 +1,15 @@
+package exer;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36-0700] \"GET /apache_pb.gif HTTP/1.0\" 200";
+
+        int start = log.indexOf("/", log.indexOf("\""));
+        int end = log.indexOf(" HTTP", start);
+
+        String url = log.substring(start, end);
+
+        System.out.println(url);
+    }
+}
